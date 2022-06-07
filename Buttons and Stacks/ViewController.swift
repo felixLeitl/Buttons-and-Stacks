3aace69afccd6fb142c99ba3ef81baac958b9147
Felix Leitl <felix.leitl@icloud.com>
2022-06-08 01:24:23 +0200

    Colors change

--- Buttons and Stacks/ViewController.swift
+++ Buttons and Stacks/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorLable: UILabel!
+    
+    var color: [UIColor] = [.red, .orange, .yellow, .green, .blue, .systemIndigo, .systemPurple]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    @IBAction func colorButtonPressed(_ sender: UIButton) {
+        //        print("You clicked tag #: \(sender.tag)")
+        //        print(sender.currentTitle!)
+        
+        if sender.tag >= 0 && sender.tag <= 6{
+            colorLable.text = "You clicked \(sender.currentTitle!)"
+            colorLable.textColor = color[sender.tag]
+        }else{
+            colorLable.text = ""
+        }
+        
+    }
+    
+    
 }
 
